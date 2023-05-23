7e0ec279111c5f60b11500b40975a74f538089e3
Romoramel <romoramel@hotmail.com>
2023-05-23 00:27:33 -0700

    week 4 completed, first push

--- interview-questions.md
+++ interview-questions.md
@@ -8,42 +8,43 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is object-oriented programming? How is it different than functional programming?
 
-Your answer:
+Your answer: object-oriented programming a style of programming used mostly wuth object and functional programming is the same but used with functions
 
-Researched answer:
+Researched answer: OOP is a programming paradigm based on the concept of objectsthat can contain data or code and functional programming is the same but by making functions
 
 2. What is the difference between a Float and an Integer in Ruby?
 
-Your answer:
+Your answer: float is like a remainder, like a number that has a decimal and integer is a full number with no decimals 
 
-Researched answer:
+Researched answer: a float in a Ruby is a number that contains a decimal point and
+Ruby integers are positive, negative, or zero numbers that do not have a fractional part
 
 3. Ruby has an implicit return. What does that mean?
 
-Your answer:
+Your answer: not needed a return keyword
 
-Researched answer:
+Researched answer: it means that if a return is the last expression in a method then there's no need for the return keyword
 
 4. What is a block in Ruby?
 
-Your answer:
+Your answer: a chunk of code
 
-Researched answer:
+Researched answer: a block of code that is always enclosed within braces
 
 5. How do you extract a value in a Ruby hash?
 
-Your answer:
+Your answer: by using the bracket notaion
 
-Researched answer:
+Researched answer: the values in a hash can be accessed using bracket notation. by typing the key in square brackets you access the value.
 
 ## Looking Ahead: Terms for Next Week
 
-1. RSpec:
+1. RSpec: a testing tool for behavior driven developement 
 
-2. Test-driven development:
+2. Test-driven development: a process where you start to write the test cases before the code is complete, then add more code in order to pass all test cases.
 
-3. PostgreSQL:
+3. PostgreSQL: is an open source database used in ruby rails
 
-4. CRUD:
+4. CRUD: CREATE READ UPDATE AND DEELETE
 
-5. HTTP:
+5. HTTP: HTTP means hyper text transfer protocol 
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -3,6 +3,11 @@
 
 # --------------------1) Create a method that takes in a number and determines if the number is even or odd. Use the test variables provided.
 
+# pseudo code
+# input: a method that takes in a number
+# output: determines if the number is even or odd
+# used the modulo operator to determine is the nuber is even or odd. If the remainder divided by 2 is 0 then the number is even, if not then it is odd
+
 reposts1 = 7
 # Expected output: '7 is odd'
 reposts2 = 42
@@ -10,10 +15,30 @@ reposts2 = 42
 reposts3 = 221
 # Expected output: '221 is odd'
 
+def even_or_odd(num)
+    if num % 2 == 0
+      return "#{num} is even"
+    else
+      return "#{num} is odd"
+    end
+  end
 
+  reposts1 = 7
+puts even_or_odd(reposts1) 
+
+    reposts2 = 42
+puts even_or_odd(reposts2)
+
+    reposts3 = 221
+puts even_or_odd(reposts3)
 
 # -------------------2) Create a method that takes in a string and removes all the vowels from the string. Use the test variables provided. 
 # HINT: Check out this resource: https://ruby-doc.org/core-2.6/String.html#method-i-delete
+
+# pseudo code
+# input: a method that takes in a string
+# output: string with vowels removed
+# used the gsub method that looks for all occurrences of a given pattern in a string and replaces them with a given replacement. the regex pattern and the /i modifier looked for the vowels in the string 
 
 beatles_album1 = 'Rubber Soul'
 # Expected output: 'Rbbr Sl'
@@ -22,6 +47,18 @@ beatles_album2 = 'Sgt Pepper'
 beatles_album3 = 'Abbey Road'
 # Expected output: 'bby Rd'
 
+def remove_vowels(string)
+    string.gsub(/[aeiou]/i, '')
+  end
+  
+beatles_album1 = 'Rubber Soul'
+  puts remove_vowels(beatles_album1) # Output: 'Rbbr Sl'
+  
+beatles_album2 = 'Sgt Pepper'
+  puts remove_vowels(beatles_album2) # Output: 'Sgt Pppr'
+  
+beatles_album3 = 'Abbey Road'
+  puts remove_vowels(beatles_album3) # Output: 'bby Rd'
 
 
 # -------------------3) Create a method that takes in a string and checks if the string is a palindrome. A palindrome is the same word spelled forward or backward. Use the test variables provided.
@@ -32,3 +69,22 @@ palindrome_test_case2 = 'LEARN'
 # Expected output: 'LEARN is not a palindrome'
 palindrome_test_case3 = 'Rotator'
 # Expected output: 'Rotator is a palindrome'
+
+def is_palindrome(string)
+    string.downcase == string.downcase.reverse ? "#{string} is a palindrome" : "#{string} is not a palindrome"
+  end
+
+
+  palindrome_test_case1 = 'Racecar'
+puts is_palindrome(palindrome_test_case1)
+
+    palindrome_test_case2 = 'LEARN'
+puts is_palindrome(palindrome_test_case2)
+
+    alindrome_test_case3 = 'Rotator'
+puts is_palindrome(palindrome_test_case3)
+
+# pseudo code
+# input: a method that takes in a string
+# output: checking if the string is a palindrome
+# used the downcase method to convert the string to loswercase to compare the reverse version of it and the reverse method reversed the string. i used the == operator to check if the original and the reversed strings are the same
